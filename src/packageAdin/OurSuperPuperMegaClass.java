c0d0bcba1aaffbf2ca4b7bb503bc236a2fb752c5
NatalyPutsko <natalyaputsko@gmail.com>
2016-09-22 18:16:24 +0300

    Initial commit

---
+++ src/packageAdin/OurSuperPuperMegaClass.java
@@ -0,0 +1,11 @@
+package packageAdin;
+
+public class OurSuperPuperMegaClass {
+
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 4;
+        System.out.println(a%b);
+    }
+
+}
